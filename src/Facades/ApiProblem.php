e9e0edaec85585c6ea71a9a06bb7b4caef4a4980
Justin Frydman <justin.frydman@gmail.com>
2023-07-05 10:55:15 -0600

    Add facade autocompletion

--- src/Facades/ApiProblem.php
+++ src/Facades/ApiProblem.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace ApiSkeletons\Laravel\ApiProblem\Facades;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array toArray()
+ * @method static JsonResponse response(mixed[] ...$params)
+ * @method static \ApiSkeletons\Laravel\ApiProblem\ApiProblem setDetailIncludesStackTrace(bool $flag)
+ */
 class ApiProblem extends Facade
 {
     protected static function getFacadeAccessor(): string
